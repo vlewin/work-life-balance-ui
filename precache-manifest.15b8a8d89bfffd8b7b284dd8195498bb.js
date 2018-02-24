46576d81483e51727e1d693ea79e1ca969552791
Vladislav Lewin <vlewin@suse.de>
2018-02-24 10:18:41 +0100

    New version

---
+++ precache-manifest.15b8a8d89bfffd8b7b284dd8195498bb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6517d6666da737d7aa65",
+    "url": "./js/vendor.6517d666.js"
+  },
+  {
+    "revision": "27e3dd7809fdcb1943e5",
+    "url": "./js/manifest.27e3dd78.js"
+  },
+  {
+    "revision": "71779e4013ce5f8abbbb",
+    "url": "./js/app.71779e40.js"
+  },
+  {
+    "revision": "d1f989a2569e9fd840bae9b92f05e4da",
+    "url": "./index.html"
+  },
+  {
+    "revision": "71779e4013ce5f8abbbb",
+    "url": "./css/app.f8cf07a4.css"
+  }
+];
