7cee9c39c6fff0331aaf98a81da858407d37fa00
Vladislav Lewin <vlewin@suse.de>
2018-05-19 12:40:25 +0200

    Move playground components to separate directory

---
+++ src/components/playground/Playground.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <button-group :buttons="buttons"></button-group>
+  </div>
+</template>
+
+<script>
+import ButtonGroup from './ButtonGroup.vue'
+export default {
+  name: "Playground",
+  components: { ButtonGroup },
+  data() {
+    return {
+      buttons: [{ name: 'Add', color: 'blue' }, { name: 'Remove', color: 'red' }]
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
