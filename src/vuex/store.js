ca8bf1f851285837a98531e51825d68a4045bc8e
Vladislav Lewin <vlewin@suse.de>
2018-07-20 20:38:56 +0200

    fix missing json loader

--- src/vuex/store.js
+++ src/vuex/store.js
@@ -20,7 +20,7 @@ const store = new Vuex.Store({
     balance: { total: 0, vacation: 0, sickness: 0 },
     debug: false,
     message: null,
-    profile: JSON.parse(localStorage.getItem('profile')),
+    profile: localStorage.profile,
     baseUri: process.env.VUE_APP_BASE_URI
   },
 
